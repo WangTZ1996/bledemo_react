2c4ae519e6cdb3a437175ddd6b2d256ef68825af
WangTZ1996 <2249796929@qq.com>
2022-03-12 10:50:23 +0800

    init project，ble connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { Spline } from 'react-spline'
 import './App.css';
+import DEMO from './assets/demo.spline'
+import SCENE from './assets/scene.json'
+// import { Application } from './pages/runtime.js';
 
 function App() {
+  console.log(SCENE, 'SCENE')
+
+  const [bleServer, setBleServer] = useState();
+
+  const initBluetooth = (namePrefix, uuid) => {
+    navigator.bluetooth
+      .requestDevice({
+        // acceptAllDevices: true,
+        filters: [{ namePrefix, }],
+        optionalServices: [uuid],
+      })
+      .then((res) => {
+        res.gatt.connect().then((res) => {
+          res
+            .getPrimaryService(uuid)
+            .then((res) => {
+              res.getCharacteristics().then((res) => {
+                console.log("ble server connect success");
+                setBleServer(res[0])
+              });
+            });
+        });
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='container'>
+      <canvas id="canvas3d"></canvas>
+      <div style={{ width: '20px', height: '20px'}} onClick={initBluetooth.bind(this, 'HC-08', '0000ffe0-0000-1000-8000-00805f9b34fb')} />
+      <Spline scene={SCENE} />
     </div>
   );
 }
